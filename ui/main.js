2a99c8bf01678946eae7e332d900a15d38543129
philippaul1998 <pippu98@gmail.com>
2017-08-06 12:48:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var element = document.getElementById('text');
+element.innerHTML='Philip';
+var img= document.getElementById('img');
+var marginRight=0;
+function moveLeft(){
+    marginRight+=1;
+    img.style.marginRight=marginRight+'px';
+}
+img.onclick =function(){
+  var interval = setInterval(moveLeft,5);  
+};
