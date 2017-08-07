d40a254a40ad0d1eb717bed38a4e037b776098e6
philippaul1998 <pippu98@gmail.com>
2017-08-07 22:23:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,11 +8,11 @@ button.onclick = function(){
         if(request.status===200){
              var counter=request.reponseText;
              var span=document.getElementById("num");
-             span.innerHTML=counter.toString();
+             span.innerHTML=counter;
         }  
       }  
     };
-    requst.open('GET','http://u98philips.imad.hasura-app.io/counter');
+    requst.open('GET','http://u98philips.imad.hasura-app.io/counter',true);
     request.send(null);
   
 };
