df9e287ccaca6a591c19959e4765127395ff8ebf
philippaul1998 <pippu98@gmail.com>
2017-08-09 20:56:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,6 +105,13 @@ function createtemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var names=[];
+app.get('/submit-name/:name', function (req, res) {
+  var name=req.params.name;
+  names.push(name);
+  
+  res.send(JSON.stringify(names));
+});
 
 var count=0;
 app.get('/counter', function (req, res) {
